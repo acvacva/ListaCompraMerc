d835a87b3b60241a759552cca3ab6f28c800c58d
sirivera <sirivera@N-5CG62309QR.nsn-intra.net>
2020-01-10 13:51:18 +0100

    first upload

---
+++ ListaCompraMerc/src/main/java/com/example/demoWeb/ListaCompraMercApplication.java
@@ -0,0 +1,14 @@
+package com.example.demoWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaCompraMercApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaCompraMercApplication.class, args);
+		System.out.println("Hola sharing this project");
+	}
+
+}
